fcea114e103d3a3672df84c744d95391f9d24d74
mehdiAmartiriffi <elmehdi.amratiriffi@marketingconfort.com>
2025-02-10 13:07:00 +0100

    JAXB version

--- src/main/java/org/gestiondestitresimportationbcp/service/MessageServicesDefault.java
+++ src/main/java/org/gestiondestitresimportationbcp/service/MessageServicesDefault.java
@@ -1,5 +1,6 @@
 package org.gestiondestitresimportationbcp.service;
 
+import org.gestiondestitresimportationbcp.entities.DemandeDomiciliationMessage;
 import org.gestiondestitresimportationbcp.entities.Message;
 import org.gestiondestitresimportationbcp.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +11,17 @@ import java.util.List;
 
 @Service
 public class MessageServicesDefault implements MessageServices {
-
     @Autowired
     MessageRepository messageRepository;
     @Autowired
-    private ParserDDD parserservice;
-    @Autowired
-    private ParserFile parserFile;
+    ParserFile parserFile;
 
     public  void insertMessage(){
-        Node root =  parserservice.parseDDDFile("src\\main\\resources\\static\\PORTNET_DDD_20220506160729_12650948.xml");
-        Message message = parserFile.parseMessage(root);
-        messageRepository.save(message);
+       DemandeDomiciliationMessage demandeDomiciliationMessage = parserFile.parseFile();
+       Message message = demandeDomiciliationMessage.getMessage();
+       messageRepository.save(message);
     }
-    public List<Message> selectMessages(){
+    public List<Message> selectMessages() {
       return   messageRepository.findAll();
     }
 
